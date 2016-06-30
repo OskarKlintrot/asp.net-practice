14b85fd0be6b8ef39dc1e18fd0d0d65f0dac207e
Oskar Klintrot <oskar.klintrot@gmail.com>
2016-06-30 14:44:55 +0200

    Added new projects for a Todo-app.

---
+++ Practice/Todo.Common/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Common
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
